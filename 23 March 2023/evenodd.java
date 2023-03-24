d5295776f9634f1d04558cd669045be16057712c
rohitugile <100467416+rohitugile@users.noreply.github.com>
2023-03-24 12:15:53 +0530

    Update evenodd.java

--- 23 March 2023/evenodd.java
+++ 23 March 2023/evenodd.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 class evenodd {
  public static void main(String[] args) {
     int a;
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);  // creating object of Scanner class 
 
     System.out.println("Enter the number :");   // asking for input
-    a = sc.nextInt();
+    a = sc.nextInt();      // input is strored in variable a 
 
-    if(a%2==0)
+    if(a%2==0)             // mod will return remainder if its even then remainder should be 0
     System.out.println("The number is even");
     else
     System.out.println("The number is Odd");
-    sc.close();
+    sc.close();            
  }   
 }
